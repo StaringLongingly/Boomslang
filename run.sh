7967513dc2fbaa761b3384d536ec2bc0e3b24ed5
StaringLongingly <dimbous@proton.me>
2025-05-20 12:30:46 +0300

    Fixed shell scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export CLASSPATH=".:/usr/local/lib/antlr-4.13.2-complete.jar:$CLASSPATH"
 
 javac target/Boomslang.java
-java target/Boomslang.java ./tabloid\ sc/fibonacci.tbl
+java target/Boomslang.java ./tabloid\ sc/fibonacci.tbd
--- update_antlr.sh
+++ update_antlr.sh
@@ -6,4 +6,4 @@ alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.2-complete.jar:$CLASSPA
 
 antlr4 boomslang.g4
 javac boomslang*.java
-grun boomslang init ./tabloid\ sc/fibonacci.tbl -gui
+grun boomslang init ./tabloid\ sc/fibonacci.tbd -gui
