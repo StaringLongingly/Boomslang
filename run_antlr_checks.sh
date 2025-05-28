0e1e903ebebc7cb0652513e875808b7dbb16c257
StaringLongingly <dimbous@proton.me>
2025-05-28 13:34:12 +0300

    Added script to check antlr checks

--- run_antlr_checks.sh
+++ run_antlr_checks.sh
@@ -6,4 +6,10 @@ alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.2-complete.jar:$CLASSPA
 
 antlr4 Boomslang.g4
 javac Boomslang*.java
-grun Boomslang init ./tabloid_source/sample1.tbd -gui
+
+grun Boomslang init ./tabloid_source/sample1.tbd
+grun Boomslang init ./tabloid_source/sample2.tbd
+grun Boomslang init ./tabloid_source/sample3.tbd
+grun Boomslang init ./tabloid_source/sample4.tbd
+grun Boomslang init ./tabloid_source/sample5.tbd
+grun Boomslang init ./tabloid_source/sample6.tbd
